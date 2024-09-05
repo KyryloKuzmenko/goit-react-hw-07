4509dd646e5345efdeb8e667ee4facab790a43d9
Kyrylko <kkuzmenko1992@gmail.com>
2024-09-05 17:01:25 -0230

    added db

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import { useState } from 'react'
 
-import './App.css'
+import css from "./App.module.css";
+import ContactList from "./redux/components/ContactList/ContactList";
+import ContactForm from "./redux/components/ContactForm/ContactForm";
+import SearchBox from "./redux/components/SearchBox/SearchBox";
+import { useDispatch, useSelector } from "react-redux";
+import { selectError, selectLoading } from "./redux/contactsSlice";
+import { useEffect } from "react";
+import { fetchContacts } from "./redux/contactsOps";
 
 function App() {
+  const dispatch = useDispatch();
+  const loading = useSelector(selectLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
   return (
-    <>
-    </>
-  )
+    <div>
+      <h1 className={css.title}>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      {loading && !error && <p>Loading contacts...</p>}
+      <ContactList />
+    </div>
+  );
 }
 
-export default App
+export default App;
